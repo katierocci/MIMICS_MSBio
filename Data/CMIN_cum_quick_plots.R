d1fd788137f09adac9708c0a7cfd9cff1bd67c1d
Unknown <drk.pierson@gmail.com>
2022-10-20 14:19:31 -0600

    Updat from laptop

--- Data/CMIN_cum_quick_plots.R
+++ Data/CMIN_cum_quick_plots.R
@@ -16,15 +16,15 @@ ggplot(df, aes(x=as.character(temp.trt), y=cumulativeCO2Flux, fill = as.factor(m
   facet_wrap(~ site)
 
 
-ggplot(df, aes(x=as.factor(site), y=cumulativeCO2Flux, fill = as.factor(moisture.trt))) +
-  geom_boxplot() + 
-  theme_bw() +
-  ylim(0,6) +
-  xlab("Temperature Group (deg C)") +
-  ylab("Cumulative CO2 Flux (mg C/ g soil)") +
-  facet_wrap(~ as.factor(temp.trt))
+# ggplot(df, aes(x=as.factor(site), y=cumulativeCO2Flux, fill = as.factor(moisture.trt))) +
+#   geom_boxplot() + 
+#   theme_bw() +
+#   ylim(0,6) +
+#   xlab("Temperature Group (deg C)") +
+#   ylab("Cumulative CO2 Flux (mg C/ g soil)") +
+#   facet_wrap(~ as.factor(temp.trt))
 
-ggplot(df %>% filter(moisture.trt == 60), aes(x=as.character(temp.trt), y=cumulativeCO2Flux, fill = as.factor(moisture.trt))) +
+ggplot(df %>% filter(moisture.trt == 60), aes(x=as.character(temp.trt), y=cumulativeCO2Flux, fill = as.factor(temp.trt))) +
   geom_boxplot() + 
   theme_bw() +
   ylim(0,6) +
