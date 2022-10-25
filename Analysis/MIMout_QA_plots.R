17d2d36f2b35c3bcd58421f36bd762641de4da9a
Unknown <drk.pierson@gmail.com>
2022-10-25 09:49:00 -0600

    Updat

--- Analysis/MIMout_QA_plots.R
+++ Analysis/MIMout_QA_plots.R
@@ -26,13 +26,14 @@ dfA15_lit_limit <- df %>% filter(SITE == "A") %>%
   filter(MAT == 25) %>% 
   filter(CLAY==10) %>%
   filter(LIG == 10) %>%
-  filter(LIT_tot > 60)
+  filter(LIT_tot > 50) %>%
+  filter(LIT_tot < 90)
 
-pairs(dfA15_lit_limit[,c(25, 11,12, 25,6,7,10,9, 21:24)],
-      col = alpha("black", 0.1), # Change color
+pairs(dfA15_lit_limit[,c(25, 11,12, 26,6,7,10,9, 21:24)],
+      col = alpha("black", 0.2), # Change color
       pch = 16, # Change shape of points
       cex=1.5,
       labels = c("CO2_frac_of_tot", "CO2-r", "CO2-K", "LIT", "MICr", "MICK", "SOMa", "SOMc", "Vslope", "Vint", "Kslope", "Kint"), # Change labels of diagonal
-      main = "n=1000 Random Parameter Set \nLimited to runs that consumed <40% of litter",
+      main = "n=1000 Random Parameter Set \nLimited to runs that consumed 10-40% of litter",
       upper.panel = NULL,
       breaks=c(0,0.2))
